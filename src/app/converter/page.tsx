43205a357aaff6f0c77824e736b5c990f1d3b3a8
hash-luk <lucas.patrick2506@gmail.com>
2025-05-06 21:59:21 -0300

    Added currency type, added warning in home and create a new page to convert

---
+++ src/app/converter/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useState, useEffect } from "react";
+import Form from "next/form";
+import { Currency } from "@/types/currency";
+
+export default function Converter() {
+  const [currencys, setCurrencys] = useState<Currency[]>([]);
+
+  useEffect(() => {
+    const fetchCurrencys = async () => {
+      const res = await fetch(
+        "https://openexchangerates.org/api/currencies.json"
+      );
+      const data: Record<string, string> = await res.json();
+      const formatted = Object.entries(data).map(([code, description]) => ({
+        code,
+        description,
+      }));
+
+      setCurrencys(formatted);
+    };
+
+    fetchCurrencys();
+  }, []);
+
+  return (
+    <Form action="/convert">
+      <select name="from">
+        {currencys.map((currency, idx) => (
+          <option key={idx} value={currency.code}>
+            {currency.code}
+          </option>
+        ))}
+      </select>
+      <select name="to">
+        {currencys.map((currency, idx) => (
+          <option key={idx} value={currency.code}>
+            {currency.code}
+          </option>
+        ))}
+      </select>
+      <input name="money-value" />
+      <button type="submit">Converter</button>
+    </Form>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,7 @@
-"use client";
-import { useState, useEffect } from "react";
-import Form from "next/form";
-
-type Currency = {
-  code: string;
-  description: string;
-};
-
 export default function Home() {
-  const [currencys, setCurrencys] = useState<Currency[]>([]);
-
-  useEffect(() => {
-    const fetchCurrencys = async () => {
-      const res = await fetch(
-        "https://openexchangerates.org/api/currencies.json"
-      );
-      const data: Record<string, string> = await res.json();
-      const formatted = Object.entries(data).map(([code, description]) => ({
-        code,
-        description,
-      }));
-
-      setCurrencys(formatted);
-    };
-
-    fetchCurrencys();
-  }, []);
-
   return (
-    <Form action="/convert">
-      <select name="from">
-        {currencys.map((currency, idx) => (
-          <option key={idx} value={currency.code}>
-            {currency.code}
-          </option>
-        ))}
-      </select>
-      <select name="to">
-        {currencys.map((currency, idx) => (
-          <option key={idx} value={currency.code}>
-            {currency.code}
-          </option>
-        ))}
-      </select>
-      <input name="money-value" />
-      <button type="submit">Converter</button>
-    </Form>
+    <div className="w-full h-[100vh] bg-red text-center flex items-center justify-center">
+      <h1 className="text-white text-6xl">🚧 In progress 🚧</h1>
+    </div>
   );
 }
